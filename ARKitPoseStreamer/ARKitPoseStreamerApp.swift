ab8ebc2d03651f12548dde8d03944f3a11a95d93
jayyoung0802 <jayyoung0802@gmail.com>
2026-04-08 13:33:34 +0800

    Initial Commit

---
+++ ARKitPoseStreamer/ARKitPoseStreamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ARKitPoseStreamerApp.swift
+//  ARKitPoseStreamer
+//
+//  Created by chosenone on 2026/4/8.
+//
+
+import SwiftUI
+
+@main
+struct ARKitPoseStreamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
